3dec65366cb561f75a0321e867fb2148f5fbe9e8
Alison Golden <alisong779@yahoo.com>
2019-03-27 14:53:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,12 +10,12 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-# #  binding.pry
-#   while number > 0
-#     puts "#{number} SECOND(S)!" 
-#     sleep(1)
-#     number -= 1
-#   end
-# "HAPPY NEW YEAR!"
-# end
+#  binding.pry
+  while number > 0
+    puts "#{number} SECOND(S)!" 
+    sleep(1)
+    number -= 1
+  end
+"HAPPY NEW YEAR!"
+end
 
