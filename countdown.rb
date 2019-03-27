823797dac4018515cc3e75f9e4560e716cde724b
Alison Golden <alisong779@yahoo.com>
2019-03-27 14:53:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,7 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-# #def countdown_with_sleep(number)
+def countdown_with_sleep(number)
 # #  binding.pry
 #   while number > 0
 #     puts "#{number} SECOND(S)!" 
